bcc2c935cf872a551856fe3f815f5255445e767f
angel <sillyangel3@gmail.com>
2024-11-21 01:42:17 +0000

    Upgrade Next.js to version 15 and update TypeScript configuration; modify PWA manifest details and add new icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,24 +2,43 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Next.js',
+    name: 'Offbrand Spotify',
+    short_name: 'Offbrand',
+    description: 'a very offbrand spotify clone',
     start_url: '/',
+    categories: ["music", "entertainment"],
+    display_override: ['window-controls-overlay'],
     display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    background_color: '#0f0f0f',
+    theme_color: '#0f0f0f',
     icons: [
-      {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+      { 
+         src: './public/favicon.ico', 
+         type: 'image/x-icon', 
+         sizes: '16x16 32x32'
       },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+      { 
+         src: './public/icon-192.png', 
+         type: 'image/png', 
+         sizes: '192x192' 
       },
-    ],
+      { 
+         src: './public/icon-512.png', 
+         type: 'image/png', 
+         sizes: '512x512' 
+      },
+      { 
+         src: './public/icon-192-maskable.png', 
+         type: 'image/png', 
+         sizes: '192x192', 
+         purpose: 'maskable' 
+      },
+      { 
+         src: './public/icon-512-maskable.png', 
+         type: 'image/png', 
+         sizes: '512x512', 
+         purpose: 'maskable'
+      }
+   ],
   }
 }
