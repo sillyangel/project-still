b85542123d07b8ad202202e0bf4b4a748ceb232e
sillyangel <sillyangel3@gmail.com>
2024-11-27 15:56:46 -0600

    Add play/pause functionality and album cover display to AudioPlayer component

--- app/components/AudioPlayer.tsx
+++ app/components/AudioPlayer.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 import { useAudioPlayer } from '@/app/components/AudioPlayerContext';
+import { FaPlay, FaPause } from "react-icons/fa6";
 
 export const AudioPlayer: React.FC = () => {
   const { currentTrack } = useAudioPlayer();
   const audioRef = useRef<HTMLAudioElement>(null);
   const [progress, setProgress] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     if (currentTrack && audioRef.current) {
       audioRef.current.src = currentTrack.url;
       audioRef.current.play();
+      setIsPlaying(true);
     }
   }, [currentTrack]);
 
@@ -40,25 +44,34 @@ export const AudioPlayer: React.FC = () => {
     }
   };
 
+  const togglePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
   return (
     <div className="fixed bottom-0 w-9/12 bg-hover text-white p-4 rounded-2xl mb-2 mx-2">
       {currentTrack ? (
         <div className="flex items-center">
-          <img src={currentTrack.image} alt={currentTrack.name} className="w-16 h-16 mr-4" />
+          <Image src={currentTrack.image} alt={currentTrack.name} width={64} height={64} className="w-16 h-16 mr-4" />
           <div className="flex-1">
             <p>{currentTrack.name} by {currentTrack.artists.join(', ')}</p>
-            <div
-              className="w-full h-2 bg-gray-300 rounded-full cursor-pointer"
-              onClick={handleProgressClick}
-            >
+            <div className="w-full h-2 bg-gray-300 rounded-full cursor-pointer mt-2" onClick={handleProgressClick}>
               <div
                 className="h-full bg-blue-500 rounded-full"
                 style={{ width: `${progress}%` }}
               />
             </div>
           </div>
-          <button onClick={() => audioRef.current?.play()}>Play</button>
-          <button onClick={() => audioRef.current?.pause()}>Pause</button>
+          <button onClick={togglePlayPause}>
+            {isPlaying ? <FaPause /> : <FaPlay />}
+          </button>
         </div>
       ) : (
         <p>No track playing</p>
